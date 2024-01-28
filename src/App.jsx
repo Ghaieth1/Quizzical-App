4be40d742f99350d5fe073fe8a40350d4cd03886
Grami Ghaieth <ghaieth.grami06@gmail.com>
2024-01-28 07:59:55 +0100

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-function App() {
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+import Questions from "./components/Questions";
+// import Answers from "./components/Answers";
+
+const Home = () => {
   return (
-    <div>
-      <h1 className="bg-red-600">HELLO</h1>
+    <div className="flex flex-col items-center justify-center h-screen bg-blue-100 ">
+      <h1 className="text-6xl font-bold mb-10">Quizzical</h1>
+
+      <Link to="/questions">
+        <button className="px-16  py-5 mt-4 text-white bg-blue-500 rounded-2xl text-2xl ">
+          Start quiz
+        </button>
+      </Link>
     </div>
   );
-}
+};
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/questions" element={<Questions />} />
+      {/* <Route path="/answers" element={<Answers />} /> */}
+    </Routes>
+  </Router>
+);
 
 export default App;
---
+++ src/components/Questions.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Questions = () => {
+  return <div>HELLO</div>;
+};
+
+export default Questions;
