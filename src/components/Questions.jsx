2e962f1055e48486e3cb65cf94fd635ec13c3546
Ghaieth Grami <ghaieth.grami06@gmail.com>
2024-02-06 13:59:51 +0100

    label style

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Questions = () => {
   const [questions, setQuestions] = useState([]);
@@ -7,34 +7,34 @@ const Questions = () => {
   const [answersChecked, setAnswersChecked] = useState(false);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple"
-        );
-        const data = await response.json();
-
-        if (data.results) {
-          setQuestions(
-            data.results.map((question) => ({
-              ...question,
-              answers: shuffleArray(
-                question.incorrect_answers.concat(question.correct_answer)
-              ),
-            }))
-          );
-        } else {
-          console.error(
-            "Erreur lors de la récupération des questions : les résultats ne sont pas présents dans la réponse de l'API."
-          );
-        }
-      } catch (error) {
-        console.error("Erreur lors de la récupération des questions :", error);
-      }
-    };
-
-    fetchData();
+    fetchQuestions();
   }, []);
+
+  const fetchQuestions = async () => {
+    try {
+      const response = await fetch(
+        "https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple"
+      );
+      const data = await response.json();
+
+      if (data.results) {
+        setQuestions(
+          data.results.map((question) => ({
+            ...question,
+            answers: shuffleArray(
+              question.incorrect_answers.concat(question.correct_answer)
+            ),
+          }))
+        );
+      } else {
+        console.error(
+          "Erreur lors de la récupération des questions : les résultats ne sont pas présents dans la réponse de l'API."
+        );
+      }
+    } catch (error) {
+      console.error("Erreur lors de la récupération des questions :", error);
+    }
+  };
 
   const handleAnswerSelect = (index, answer) => {
     const newSelectedAnswers = [...selectedAnswers];
@@ -86,52 +86,67 @@ const Questions = () => {
     return shuffledArray;
   };
 
-  return (
-    <div className="mx-auto my-5 space-y-5 text-center">
-      {questions.map((question, index) => (
-        <div key={index}>
-          <h1>{question.question.replace(/&quot;/g, '"')}</h1>
+  const resetQuiz = () => {
+    setAnswersChecked(false);
+    setSelectedAnswers(Array(10).fill(null));
+    setScore(null);
+    fetchQuestions();
+  };
 
-          <div className="flex justify-center gap-5">
-            {question.answers.map((answer, i) => (
-              <label
-                key={i}
-                className={`block w-48 my-2 text-center cursor-pointer`}
-              >
-                <input
-                  type="radio"
-                  name={`answer-${index}`}
-                  value={answer}
-                  checked={selectedAnswers[index] === answer}
-                  onChange={() => handleAnswerSelect(index, answer)}
-                  className="hidden"
-                />
-                <div
-                  className={`px-4 py-2 font-bold rounded-full text-white bg-blue-500 hover:bg-violet-700 ${getAnswerClass(
-                    index,
-                    answer
-                  )}`}
+  return (
+    <div className="bg-slate-200 h-screen ">
+      <div className="mx-auto p-10  text-center ">
+        {questions.map((question, index) => (
+          <div key={index}>
+            <h1
+              className="mb-5 font-bold"
+              dangerouslySetInnerHTML={{ __html: question.question }}
+            ></h1>
+
+            <div className="flex justify-center gap-5">
+              {question.answers.map((answer, i) => (
+                <label
+                  key={i}
+                  className={`block w-48 my-2 text-center cursor-pointer mb-5`}
                 >
-                  {answer.replace(/&quot;/g, '"')}
-                </div>
-              </label>
-            ))}
+                  <input
+                    type="radio"
+                    name={`answer-${index}`}
+                    value={answer}
+                    checked={selectedAnswers[index] === answer}
+                    onChange={() => handleAnswerSelect(index, answer)}
+                    className="hidden"
+                  />
+                  <div
+                    className={`px-4 py-2 font-bold rounded-full text-white bg-blue-500 hover:bg-violet-700 ${getAnswerClass(
+                      index,
+                      answer
+                    )}`}
+                    dangerouslySetInnerHTML={{ __html: answer }}
+                    style={{
+                      boxShadow:
+                        "10px 10px 5px -5px black, 5px 10px 10px -5px black",
+                    }}
+                  ></div>
+                </label>
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
-      <div className="flex justify-center">
-        <button
-          className="px-4 py-2 mt-4 text-white bg-blue-500 hover:bg-violet-800 rounded-2xl"
-          onClick={verifyAnswers}
-        >
-          Check answers
-        </button>
-      </div>
-      {score !== null && (
+        ))}
         <div className="flex justify-center">
-          <h1>{`You Scored ${score} sur ${questions.length}`}</h1>
+          <button
+            className="px-4 py-2 mt-4 text-white bg-blue-500 hover:bg-violet-800 rounded-2xl"
+            onClick={answersChecked ? resetQuiz : verifyAnswers}
+          >
+            {answersChecked ? "Try again" : "Check answers"}
+          </button>
         </div>
-      )}
+        {score !== null && (
+          <div className="flex justify-center p-5 font">
+            <h1>{`You Scored ${score} sur ${questions.length}`}</h1>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
